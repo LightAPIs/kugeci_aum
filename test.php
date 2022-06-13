910ea95fb07b2e415eb94ea74c616bfc10f1926c
Light <47911535+LightAPIs@users.noreply.github.com>
2022-06-13 18:10:03 +0800

    test: 重构测试方法

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+require('debug.php');
+require('src/kuSource.php');
+
+$downloader = (new ReflectionClass('AumKuSource'))->newInstance();
+$testArray = array(
+    array('title' => '谢谢你的爱', 'artist' => '刘德华')
+);
+
+foreach ($testArray as $key => $item) {
+    echo "\n++++++++++++++++++++++++++++++\n";
+    echo "测试 $key 开始...\n";
+    if ($key > 0) {
+        echo "等待 5 秒...\n";
+        sleep(5);
+    }
+    $testObj = new AudioStationResult();
+    $count = $downloader->getLyricsList($item['artist'], $item['title'], $testObj);
+    if ($count > 0) {
+        $item = $testObj->getFirstItem();
+        $downloader->getLyrics($item['id'], $testObj);
+    } else {
+        echo "没有查找到任何歌词！\n";
+    }
+    echo "测试 $key 结束。\n";
+}
